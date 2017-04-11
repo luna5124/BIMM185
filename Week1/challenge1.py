a0afd742a1d6f9c89d1096ef29ca4601564fdf73
Lihui Lu <lunarlu5124@gmail.com>
2017-04-11 16:07:04 -0700

    Week 1 Lab

--- Week1/challenge1.py
+++ Week1/challenge1.py
@@ -1,13 +1,19 @@
-with open('first2.txt','r') as file:
+import re
+with open('TCDB.faa','r') as file:
 	start = False
 	for line in file:
 		line = line[:-1]
-		if ">" in line:
+		m = re.search('>.*\|.*\|(\S+)\|(\S+)',line)
+		if m:
 			if start:
 				print()
 			else:
 				start = True
-			line = line.split('|')
-			print(line[3].split()[0] + '-' + line[2] + '\t',end='')
+
+			#line = line.split('|')
+			#print(line[3].split()[0] + '-' + line[2] + '\t',end='')
+			
+			#use regular expression
+			print(m.group(2) + '-' + m.group(1) + '\t',end='')
 		else:
 			print(line,end='')
--- Week1/challenge2.py
+++ Week1/challenge2.py
@@ -1,3 +1,8 @@
+#Please note:
+#Right is the protein in the first column(left)
+#Left is the second column(right) in the input file.
+#Since the usage of these two variables are consistent throughout the code, 
+#I decided to keep it as it was
 with open('RS.txt','r') as file:
 	right = ""
 	left = ""
@@ -10,17 +15,19 @@ with open('RS.txt','r') as file:
 		if right == line[0]:
 			count += 1
 			if float(line[-1]) > my_max:
+				#if the possiblily of the readin pair is higher than the pairs with the same reference protein
 				left = line[1]
 				my_max = float(line[-1])
 		else:
 			if my_max != 0:
-	#			print(right, left, my_max)
+				#print(right, left, my_max)
 				if count in counts:
 					counts[count].append((right, left,my_max))
 				else:
 					counts[count]=[(right, left, my_max)]
 			if protein == 2000:
 				break
+			#initilization when hit a new reference protein 
 			right = line[0]
 			my_max = float(line[-1])
 			left = line[1]
@@ -31,6 +38,7 @@ with open('RS.txt','r') as file:
         #else:
         #        counts[count]=[(right, left, my_max)]
 
+    #in decending order of the number of interecting pairs
 	for key in sorted(counts.keys(), reverse=True):
 		for i in range(len(counts[key])):
 			my_tuple = counts[key][i]
