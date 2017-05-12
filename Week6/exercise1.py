d6a39176fe8ce2f3f2a515a160db5557c52e9e84
Lihui Lu <lunarlu5124@gmail.com>
2017-05-11 17:41:03 -0700

    before class commit

--- Week6/exercise1.py
+++ Week6/exercise1.py
@@ -8,7 +8,7 @@ database = 'bimm185'
 
 def main():
 	myConnection = pymysql.connect(host=hostname, user=username, passwd=password, db=database, local_infile=True, autocommit=True)
-
+	count = 0
 	with open('TUSet.txt','r') as file:
 		for line in file:
 			if '#' in line:
@@ -19,12 +19,51 @@ def main():
 			gene_name = line[3]
 			confidence = line[6]
 			if confidence == 'Strong' or confidence == 'Confirmed':
-				#print(gene_name)
+				count += 1
+				print(gene_name)
 				gene_name = gene_name.split(',')
+				if len(gene_name) < 2:
+					continue
+				pos = []
 				for g in gene_name:
+					if "<" in g:
+						#the gene name is malformed
+						continue
 					#print('hello')
-					print(query_gene_name(myConnection, g))
+					result = query_gene_name(myConnection, g)
+					#print(g)
+					if result is None:
+						print("Try finding the gene through synonym table: ", g)
+						result = query_gene_synonym(myConnection, g)
 
+					if result is not None:
+						exons = query_exon(myConnection, result)
+						gene_lefts = []
+						gene_rights = []
+						for e in exons:
+							gene_lefts.append(e[1])
+							gene_rights.append(e[2])
+						pos.append((min(gene_lefts), max(gene_rights)))
+				pos.sort()
+				print(pos)
+	print(count)
+
+
+
+
+
+def query_gene_synonym(conn, name):
+	cur = conn.cursor()
+	sql_statement = ("SELECT gene_id FROM gene_synonyms WHERE synonym = '{name}'".format(name=name))
+
+	cur.execute(sql_statement)
+
+	result = cur.fetchone()
+
+	if result is None:
+		return None
+	else:
+		return result[0]
 
 
 def query_gene_name(conn, name):
@@ -38,6 +77,16 @@ def query_gene_name(conn, name):
 		return None
 	else:
 		return result[0]
+
+def query_exon(conn, gene):
+	cur = conn.cursor()
+	sql_statement = ("SELECT gene_id, left_position, right_position FROM exons WHERE gene_id = '{gene}'".format(gene=gene))
+	cur.execute(sql_statement)
+	result = cur.fetchall()
+	#print(gene)
+	#print(result)
+	return result
+
 
 if __name__ == '__main__':
 	main()
